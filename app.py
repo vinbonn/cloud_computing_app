f1d538c793ce4d687d7fb4735a4f9ab678fa7f04
vinbonn <113992194+vinbonn@users.noreply.github.com>
2024-12-11 20:37:57 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, Response
+from flask_cors import CORS
 import subprocess
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/start-wireguard', methods=['POST'])
 def start_wireguard():
